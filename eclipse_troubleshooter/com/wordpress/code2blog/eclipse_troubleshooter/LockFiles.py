c8e31edfe1a6a1476b64a38980ef297a6df42f09
vishnu <vishnu31122013@gmail.com>
2020-07-04 18:20:02 +0530

    et1 print one line at a time

--- eclipse_troubleshooter/com/wordpress/code2blog/eclipse_troubleshooter/LockFiles.py
+++ eclipse_troubleshooter/com/wordpress/code2blog/eclipse_troubleshooter/LockFiles.py
@@ -1,8 +1,22 @@
 import os
+
 # com\wordpress\code2blog\eclipse_troubleshooter
 projectFolder = '../../../../'
-fileAbsPath = os.path.abspath(projectFolder + 'resources/input.txt')
-print('file absolute path = ' + fileAbsPath)
 
-f = open(fileAbsPath, 'r')
-print(f.read())
+def simple_file_read():
+    fileAbsPath = os.path.abspath(projectFolder + 'resources/input.txt')
+    print('file absolute path = ' + fileAbsPath)
+
+    f = open(fileAbsPath, 'r')
+    print(f.read())
+
+def print_from_list():
+    fileAbsPath = os.path.abspath(projectFolder + 'resources/input.txt')
+    f = open(fileAbsPath, 'r')
+    list = f.readlines()
+    for index, line in enumerate(list):
+        print( "line number=[{0}], {1}".format(index, line))
+
+if __name__ == '__main__':
+    print_from_list()
+
