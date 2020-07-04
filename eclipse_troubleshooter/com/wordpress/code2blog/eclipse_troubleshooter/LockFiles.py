88b0b998c8ad15a714084f6888d6fca271e7b7b8
vishnu <vishnu31122013@gmail.com>
2020-07-04 19:38:41 +0530

    et1 using python i found lock files to delete

--- eclipse_troubleshooter/com/wordpress/code2blog/eclipse_troubleshooter/LockFiles.py
+++ eclipse_troubleshooter/com/wordpress/code2blog/eclipse_troubleshooter/LockFiles.py
@@ -1,7 +1,9 @@
 import os
+import re
 
 # com\wordpress\code2blog\eclipse_troubleshooter
 projectFolder = '../../../../'
+
 
 def simple_file_read():
     fileAbsPath = os.path.abspath(projectFolder + 'resources/input.txt')
@@ -10,13 +12,30 @@ def simple_file_read():
     f = open(fileAbsPath, 'r')
     print(f.read())
 
+
 def print_from_list():
     fileAbsPath = os.path.abspath(projectFolder + 'resources/input.txt')
     f = open(fileAbsPath, 'r')
     list = f.readlines()
     for index, line in enumerate(list):
-        print( "line number=[{0}], {1}".format(index, line))
+        print("line number=[{0}], {1}".format(index, line))
+
+
+def find_lock_files():
+    fileAbsPath = os.path.abspath(projectFolder + 'resources/lock_file_paths.txt')
+    f = open(fileAbsPath, 'r')
+    list = f.readlines()
+    for index, line in enumerate(list):
+        # sanitize input passed in through config file - lock_file_paths.txt
+        line = line.strip()
+        if(not line.endswith('/') and not line.endswith("\\")):
+            line = line + '/'
+        #
+        # print("line {0} from config file = {1}".format(index, line))
+        lock_files = [any_file for any_file in os.listdir(line) if re.match(r'.*Lock', any_file)]
+        for file in lock_files:
+            abs_lock_file = line + file
+            print("lock file found : {0}".format(abs_lock_file[-100:]))
 
 if __name__ == '__main__':
-    print_from_list()
-
+    find_lock_files()
